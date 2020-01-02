ad4ef9f928a5fb867b1c88131d31123c890f979f
grobot <1157215781@qq.com>
2020-01-02 09:22:57 +0800

    add mouse event

--- src/maps/OccupancyGridClient.js
+++ src/maps/OccupancyGridClient.js
@@ -29,7 +29,56 @@ ROS2D.OccupancyGridClient = function(options) {
   this.rootObject.addChild(this.currentGrid);
   // work-around for a bug in easeljs -- needs a second object to render correctly
   this.rootObject.addChild(new ROS2D.Grid({size:1}));
+  this.panviewer = new ROS2D.PanView({
+    rootObject: this.rootObject
+  });
+  this.zoomviewer = new ROS2D.ZoomView({
+    rootObject:this.rootObject
+  });
+  this.startx = 0;
+  this.starty = 0;
+  this.zoomx = 0;
+  this.zoomy = 0;
+  this.onmove = false;
+  this.rootObject.addEventListener('stagemousedown', function(event) {
+    that.panviewer.startPan(0,0);
+    that.startx = event.stageX;
+    that.starty = event.stageY;
+    that.onmove = true;
 
+  });
+  this.rootObject.addEventListener('stagemousemove', function(event) {
+    if(that.onmove)
+    {
+
+      that.panviewer.pan(event.stageX-that.startx,event.stageY-that.starty);
+    }
+    that.zoomx = event.stageX;
+    that.zoomy = event.stageY;
+
+  });
+
+  this.rootObject.addEventListener('stagemouseup', function(event) {
+    that.onmove = false;
+  });
+  this.zoomviewin = 1;
+  this.rootObject.getStage().canvas.addEventListener('mousewheel',function(event){
+    that.zoomviewer.startZoom(that.zoomx,that.zoomy);
+    if(event.deltaY > 0)
+    {
+      that.zoomviewin = 1;
+      that.zoomviewin += 0.1;
+    }
+    else{
+      that.zoomviewin = 1;
+      that.zoomviewin -= 0.1;
+    }
+    that.zoomviewer.zoom(that.zoomviewin);
+    
+
+    console.log(event);
+  });
+  
   // subscribe to the topic
   var rosTopic = new ROSLIB.Topic({
     ros : ros,
